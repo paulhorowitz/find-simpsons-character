28635bc12f7b44efc0ff7e42191be9cd8f104401
paulhorowitz <paulbhorowitz@gmail.com>
2021-05-14 12:55:22 +0930

    listener to see where clicked

--- src/Parts/Main/image.js
+++ src/Parts/Main/image.js
@@ -2,10 +2,22 @@ import React from "react";
 import picture from '../../Assets/Images/934853.jpg'
 
 const MainSection = () => {
+  React.useEffect(() => {
+    let test = document.getElementById('clickme');
+    test.onclick = function clickEvent(e) {
+      // e = Mouse click event.
+      var rect = e.target.getBoundingClientRect();
+      var x = e.clientX - rect.left; //x position within the element.
+      var y = e.clientY - rect.top;  //y position within the element.
+      console.log("Left : " + x + " ; Top : " + y + ".");
+      
+    }
+
+  }, []);
+
   return(
-    <img className="simpsons-images" src={picture}></img>
+    <img className="simpsons-images" id='clickme' src={picture}></img>
   )
 }
-
 
 export default MainSection;
