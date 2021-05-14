f2eab19dc4581bd7c14f511e49baacd02a1c6cf2
paulhorowitz <paulbhorowitz@gmail.com>
2021-05-14 12:23:47 +0930

    Add firestore script and first image to main page

---
+++ src/Parts/Main/image.js
@@ -0,0 +1,11 @@
+import React from "react";
+import picture from '../../Assets/Images/934853.jpg'
+
+const MainSection = () => {
+  return(
+    <img className="simpsons-images" src={picture}></img>
+  )
+}
+
+
+export default MainSection;
